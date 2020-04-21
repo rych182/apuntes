666552a690e9735ca86ed8d4a0964d79680fbd32
ricgarrido <ricgarridoc@hotmail.com>
2020-04-21 16:03:16 -0500

    ES6: I added more methods

--- practicas/ejercicios-js/numerosEnJs.js
+++ practicas/ejercicios-js/numerosEnJs.js
@@ -1,4 +1,4 @@
-let resultado1 = Math.round(2.49);
+let resultado1 = Math.random();
 console.log(resultado1);
 /*
 
@@ -12,7 +12,35 @@ Ejercicio 2, Math.PI te imprime 3.1416
 let resultado = Math.PI;
 console.log(resultado);
 
-Ejercicio 2, Math.round() redondea hacia arriba o abajo
+Ejercicio 3, Math.round() redondea hacia arriba o abajo
 let resultado1 = Math.round(2.49);
+console.log(resultado1);
+
+Ejercicio 4 Math.ceil() redondea hacia arriba
+let resultado1 = Math.floor(2.49);
+console.log(resultado1);
+
+Ejercicio 5 Math.floor() redondea hacia abajo
+let resultado1 = Math.floor(2.49);
+console.log(resultado1);
+
+Ejercicio 6 Math.sqrt() saca la raíz cuadrada
+let resultado1 = Math.sqrt(144);
+console.log(resultado1);
+
+Ejercicio 6 Math.abs() te devuelve el número absoluto(lo negativo lo vuelve positivo)
+let resultado1 = Math.abs(-3);
+console.log(resultado1);
+
+Ejercicio 7 Math.pow() te eleva el número a la potencia que le digas
+let resultado1 = Math.pow(2,4);
+console.log(resultado1);
+
+Ejercicio 8 Math.min(2,4,5,6,7,9) te dice el valor mas pequeño de los números que pusiste
+let resultado1 = Math.min(2,4,5,6,7,9);
+console.log(resultado1);
+
+Ejercicio 9 Math.random() te da un valor aleatorio del 0 al 0.99
+let resultado1 = Math.random();
 console.log(resultado1);
 */
