448f027a9dba0717d405e9940d6bbe44b4c995f7
El Richo <ricgarridoc@hotmail.com>
2020-04-20 23:26:17 -0500

    General-project: I added notes about ES6

---
+++ practicas/ejercicios-js/numerosEnJs.js
@@ -0,0 +1,18 @@
+let resultado1 = Math.round(2.49);
+console.log(resultado1);
+/*
+
+Ejercicio 1: Sacar el residuo de una división
+const num1 = 20;
+const num2 = 30;
+const resultado = num2 % num1;
+console.log(resultado);
+
+Ejercicio 2, Math.PI te imprime 3.1416
+let resultado = Math.PI;
+console.log(resultado);
+
+Ejercicio 2, Math.round() redondea hacia arriba o abajo
+let resultado1 = Math.round(2.49);
+console.log(resultado1);
+*/
