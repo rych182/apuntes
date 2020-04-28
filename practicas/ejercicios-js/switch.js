ccece4048a7190c14c7521653b1483a656f111e4
ricgarrido <ricgarridoc@hotmail.com>
2020-04-28 04:17:15 -0500

    General-project: I added notes about Switch

---
+++ practicas/ejercicios-js/switch.js
@@ -0,0 +1,15 @@
+const dia = 5;
+
+switch (dia) {
+    case 0:
+        console.log("HOy es Domingo");
+        break;
+    case 1:
+        console.log("HOy es Lunes");
+        break;
+    case 2:
+        console.log("HOy es Martes");
+        break;
+    default:
+        console.log("Es otro día");
+}
