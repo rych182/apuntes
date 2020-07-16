a626136d7f5a0dbdf49eef9839b7d0d44fa8644c
ricgarrido <ricgarridoc@hotmail.com>
2020-07-16 13:11:49 -0500

    Notes about LocalStorage

--- practicas/universidad/js/app.js
+++ practicas/universidad/js/app.js
@@ -1,22 +1,29 @@
 // Eliminar de Local Storage
 //localStorage.clear();
 
-document.getElementById('submit-buscador').addEventListener('click', ejecutar);
-
-function ejecutar(e) {
-    e.preventDefault();
-    alert("Ejecutando desde una funcion");
-}
+//Agregar un local storage
+localStorage.setItem('nombre', 'Ricardo');
+sessionStorage.setItem('pais', 'Canada');
+localStorage.removeItem('nombre');
+//Agregar un sesion
 
 
 /*
 Ejercicio para seleccionar enlaces link
 const primerLi = document.querySelectorAll('.enlace')[1];
 console.log(primerLi);
-
+-------------------------------------------------------------------------------------
+Ejecutar un evento click
 document.getElementById('submit-buscador').addEventListener('click', function(e) {
     e.preventDefault();
     alert("He dado un click");
 })
+Otra forma de hacerlo
+document.getElementById('submit-buscador').addEventListener('click', ejecutar);
+function ejecutar(e) {
+    e.preventDefault();
+    alert("Ejecutando desde una funcion");
+}
+
 
 */
