a4abb0c0e917ea4588bd429ff1c8e428240b385d
ricgarrido <ricgarridoc@hotmail.com>
2020-07-16 11:55:41 -0500

    I added notes about addEventListeners

--- practicas/universidad/js/app.js
+++ practicas/universidad/js/app.js
@@ -1,5 +1,22 @@
 // Eliminar de Local Storage
 //localStorage.clear();
 
+document.getElementById('submit-buscador').addEventListener('click', ejecutar);
+
+function ejecutar(e) {
+    e.preventDefault();
+    alert("Ejecutando desde una funcion");
+}
+
+
+/*
+Ejercicio para seleccionar enlaces link
 const primerLi = document.querySelectorAll('.enlace')[1];
 console.log(primerLi);
+
+document.getElementById('submit-buscador').addEventListener('click', function(e) {
+    e.preventDefault();
+    alert("He dado un click");
+})
+
+*/
