ac079eb6321722f541bb600e702ab7a9be7a5b51
ricgarrido <ricgarridoc@hotmail.com>
2020-11-24 17:20:40 -0600

    actualizaciones

--- practicas/experimentos-bootstrap/copiando-menus/eBusiness/assets/js/main.js
+++ practicas/experimentos-bootstrap/copiando-menus/eBusiness/assets/js/main.js
@@ -10,12 +10,7 @@
     /*--------------------------
     preloader
     ---------------------------- */
-    $(window).on('load', function() {
-        var pre_loader = $('#preloader');
-        pre_loader.fadeOut('slow', function() {
-            $(this).remove();
-        });
-    });
+
 
     // Toggle .header-scrolled class to #header when page is scrolled
     $(window).scroll(function() {
@@ -102,187 +97,61 @@
     }
 
     // Activate smooth scroll on page load with hash links in the url
-    $(document).ready(function() {
-        if (window.location.hash) {
-            var initial_nav = window.location.hash;
-            if ($(initial_nav).length) {
-                var scrollto = $(initial_nav).offset().top - scrolltoOffset;
-                $('html, body').animate({
-                    scrollTop: scrollto
-                }, 1500, 'easeInOutExpo');
-            }
-        }
-    });
+
 
     /*----------------------------
      wow js active
     ------------------------------ */
-    new WOW().init();
+    //new WOW().init();
 
     //---------------------------------------------
     //Nivo slider
     //---------------------------------------------
-    $('#ensign-nivoslider').nivoSlider({
-        effect: 'random',
-        slices: 15,
-        boxCols: 12,
-        boxRows: 8,
-        animSpeed: 500,
-        pauseTime: 5000,
-        startSlide: 0,
-        directionNav: true,
-        controlNavThumbs: false,
-        pauseOnHover: true,
-        manualAdvance: false,
-    });
+
 
     /*----------------------------
      Scrollspy js
     ------------------------------ */
-    var Body = $('body');
-    Body.scrollspy({
-        target: '.navbar-collapse',
-        offset: 80
-    });
+
 
     /*---------------------
       Venobox
     --------------------- */
-    var veno_box = $('.venobox');
-    veno_box.venobox();
+
 
     /*----------------------------
     Page Scroll
     ------------------------------ */
-    var page_scroll = $('a.page-scroll');
-    page_scroll.on('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 55
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+
 
     /*--------------------------
       Back to top button
     ---------------------------- */
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-
-    $('.back-to-top').click(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 1500, 'easeInOutExpo');
-        return false;
-    });
 
     /*----------------------------
      Parallax
     ------------------------------ */
-    var well_lax = $('.wellcome-area');
-    well_lax.parallax("50%", 0.4);
-    var well_text = $('.wellcome-text');
-    well_text.parallax("50%", 0.6);
 
     /*--------------------------
      collapse
     ---------------------------- */
-    var panel_test = $('.panel-heading a');
-    panel_test.on('click', function() {
-        panel_test.removeClass('active');
-        $(this).addClass('active');
-    });
+
 
     /*---------------------
      Testimonial carousel
     ---------------------*/
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        dots: true,
-        loop: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            768: {
-                items: 1
-            },
-            900: {
-                items: 1
-            }
-        }
-    });
+
     /*----------------------------
      isotope active
     ------------------------------ */
     // portfolio start
-    $(window).on("load", function() {
-        var $container = $('.awesome-project-content');
-        $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-        });
-        var pro_menu = $('.project-menu li a');
-        pro_menu.on("click", function() {
-            var pro_menu_active = $('.project-menu li a.active');
-            pro_menu_active.removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
 
-    });
+
     //portfolio end
 
     /*---------------------
    Circular Bars - Knob
 --------------------- */
-    if (typeof($.fn.knob) != 'undefined') {
-        var knob_tex = $('.knob');
-        knob_tex.each(function() {
-            var $this = $(this),
-                knobVal = $this.attr('data-rel');
 
-            $this.knob({
-                'draw': function() {
-                    $(this.i).val(this.cv + '%')
-                }
-            });
-
-            $this.appear(function() {
-                $({
-                    value: 0
-                }).animate({
-                    value: knobVal
-                }, {
-                    duration: 2000,
-                    easing: 'swing',
-                    step: function() {
-                        $this.val(Math.ceil(this.value)).trigger('change');
-                    }
-                });
-            }, {
-                accX: 0,
-                accY: -150
-            });
-        });
-    }
 
 })(jQuery);
