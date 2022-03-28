9d8484b4c0033e3d14bc253c187e4b957a885992
ricgarrido <ricgarridoc@hotmail.com>
2022-03-28 08:31:07 -0600

    I eliminated one directory

--- practicas/webpack-practicas/webpack-starter-kit/foo/src/index.js
+++
@@ -1,4 +0,0 @@
-const arr = [1, 2, 3],
-    codeESNext = () => console.log(...arr);
-
-codeESNext();
--- practicas/webpack-practicas/webpack-starter-kit/src/index.js
+++
@@ -1,6 +0,0 @@
-import style from "./style.css";
-
-const arr = [1, 2, 3],
-    codeESNext = () => console.log(...arr);
-
-codeESNext();
