9c33db4368fccf940772a363495fe96d623d811c
ProfeRickLinux <ricgarridoc@hotmail.com>
2024-02-13 21:51:52 -0600

    Reordering js exercises with for of and for in

--- practicas/ejercicios-js/009-for-in.js
+++ practicas/ejercicios-js/009-for-in.js
@@ -1,7 +1,23 @@
 
 /*
 ========================================================================================
+Explicación FOR IN
+La elección más común para utilizar for...in en JavaScript es para iterar sobre las propiedades de un objeto.
+
+-Se utiliza para iterar sobre las propiedades enumerables de un objeto, incluyendo propiedades heredadas del prototipo.
+-Devuelve las claves (o índices en el caso de arrays) en lugar de los valores.
+
+
                                 EJERCICIOS CON FOR IN
+
+Ejercicio 0: recorrer el objeto
+let myObject = { a: 1, b: 2, c: 3 };
+
+for (let key in myObject) {
+  console.log("Clave:", key, "Valor:", myObject[key]);
+}
+
+
 EJERCICIO 1:Haz un ciclo con for in para recorrer un objeto
 let objeto = {
     nombre: "Ric",
@@ -28,4 +44,22 @@ const automovil = {
 for (const auto in automovil) {
     console.log(`${auto}: ${automovil[auto]}`);
 }
+
+----------------------------------------------------------------------------
+Ejercicio 4:
+Iterar sobre propiedades heredadas:
+
+function Animal() {
+  this.legs = 4;
+}
+
+Animal.prototype.sound = "Hiss";
+
+let cat = new Animal();
+
+for (let prop in cat) {
+  console.log(prop, cat[prop]);
+}
+
+Aquí, for...in itera sobre las propiedades del objeto cat, incluyendo las propiedades heredadas del prototipo (sound en este caso).
 */
--- practicas/ejercicios-js/010-for-of.js
+++ practicas/ejercicios-js/010-for-of.js
@@ -1,7 +1,26 @@
 /*
+For of Explicación
+
+-Se utiliza para iterar sobre elementos iterables, como arrays, strings, mapas, conjuntos, etc.
+-Proporciona una forma más sencilla y legible de recorrer los elementos de una estructura iterable.
+-No proporciona acceso directo a los índices o claves, sino a los valores de los elementos.
+
 
 ===========================================================================================
                                 EJERCICIOS CON FOR OF
+
+Ejercicio 0: explicación del porque FOR OF no proporciona el indice.
+let array = ['a', 'b', 'c'];
+
+for (let value of array) {
+  console.log("Valor:", value);
+  // ¿Cómo obtener el índice correspondiente aquí?
+}
+
+for (let index in array) {
+    console.log("Índice:", index, "Valor:", array[index]);
+  }
+
 EJERCICIO 1:Haz un ciclo con for of para recorrer un array o cadena de texto
 
 let texto = "ric";
@@ -19,5 +38,47 @@ Pero se acostumbra poner el sengular del nombre del array
 for (let country of countries) {
     console.log(country);
 }
+---------------------------------------------------------------------
+            FOR OF con funciones predefinidas
 
+const ciudades = ['Mexico', 'Canada', 'USA', 'Groelandia', 'Brasil'];
+for (let entrada of ciudades.values()) {
+    console.log(entrada);
+}
+
+Ejercicio 5, imprime los valores de un array usando FOR OF y la función .values()
+const ciudades = ['Mexico', 'Canada', 'USA', 'Groelandia', 'Brasil'];
+for (let entrada of ciudades.values()) {
+    console.log(entrada);
+}
+
+EJERCICIO 6, nos regresa solo los valores del Map, pero no los indices(llaves fue lo que dijo el profe)
+const datos = new Map();
+datos.set("nombre", "juan");
+datos.set("profesion", "desarrollo web");
+
+for (const entrada of datos.values()) {
+    console.log(entrada);
+}
+
+Ejercicio 7, usamos la función .keys() con FOR OF para conseguir el indice(o key dijo el profe)
+const ciudades = ['Mexico', 'Canada', 'USA', 'Groelandia', 'Brasil'];
+for (let entrada of ciudades.keys()) {
+    console.log(entrada);
+}
+
+Ejercicio 8, nos va a regresar nuestro indice pero en este caso el índice es nombre y profesión PORQUE ES UN OBJETO
+const datos = new Map();
+datos.set("nombre", "juan");
+datos.set("profesion", "desarrollo web");
+
+for (const entrada of datos.keys()) {
+    console.log(entrada);
+}
+
+EJERCICIO 9, imprime con un for, arrays creados con la función .entries(), usando los espacios de memoria y datos de 1 solo array
+const ciudades = ['Mexico', 'Canada', 'USA', 'Groelandia', 'Brasil'];
+for (let entrada of ciudades.entries()) {
+    console.log(entrada);
+}
 */
