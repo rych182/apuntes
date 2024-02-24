c342089559e3c49923b58c25167faa4685251c1c
ProfeRickLinux <ricgarridoc@hotmail.com>
2024-02-23 20:37:41 -0600

    adding prototype's exercises

--- practicas/ejercicios-js/025-prototipos.js
+++ practicas/ejercicios-js/025-prototipos.js
@@ -1,3 +1,4 @@
+
 /*
 JS es un lenguaje orientado a objeto basado en prototipos, no en clases
 Javascript es un lenguaje multiparadigma(estilos), coom la programación funcional, orientada a objetos.
@@ -25,8 +26,11 @@ y tiene las definiciones de sus getters y setters, es este: __proto__: Object .
 NOTA IMPORTANTE EJERCICIOS: imprimir los 4 console.log, para ver la diferencia en los objetos creados
 */
 
+//Comparo todos los objetos(los literales, con los instanciados con la palabra reservada new)
 
-//Comparo todos los objetos
+/*}
+Ejercicio 1: Comparo todos los objetos(los literales, con los instanciados con la palabra reservada new)
+
 const animal = {
     nombre: "Perro",
     accion() {
@@ -38,44 +42,69 @@ const animal2 = {
     nombre: "Gato",
     accion() {
         console.log("Maullar: Miau miau");
-
     }
 }
+
+Cuando imprimes estos 2 objetos, en la consola de navegador te aparecen sin ningún nombre porque son objetos literales
+y su prototipo es "object"
 
 console.log(animal);
 console.log(animal2);
 
 
-//Creando una función constructora
+Creando una función constructora
 function Animal(nombre, genero) {
-    //atributos
+    atributos, recibiremos su valor por el parametro
     this.nombre = nombre;
     this.genero = genero;
-    /* Mostrar cmo me marca error de esas 2 maneras
-    1-sonar(){} 
-    2-this.sonar(){}
-    */
+    // Mostrar cmo me marca error de esas 2 maneras
+    //1-sonar(){} 
+    //2-this.sonar(){}
+    
     //Metodos
+    //se pone this para que forme parte de este contructor
+    //La sintaxis de los objetos literales, no aplican cuando haces una función constructora
+    //Lo ideal, es que los metodos se saquen de la función constructora y los peguemos al prototipo
     this.sonar = function() {
-        console.log("Haciendo un sonido")
+        console.log("Haciendo un sonido y mi nombre es:"+" "+ this.nombre)
     }
 }
-
 
 //Aquí creamos la instancia con la palabra reservada "new" podría ser una instancia de tipo
 // "que le especifiquemos", puede ser "new String" ó "new Array"
 //Yo lo que quiero crear es un objeto con la función constructora "Animal"
+//const Perro = new Animal("Snoopy", "Macho");
+//const Gato = new Animal("Perlita", "Hembra");
+
+//Estos 2 objetos aparecen en la consola PERO son algo que se llama "Animal" y su función constructora es "Animal"
+//console.log(Perro);
+//console.log(Gato);
+
+//Todo esto evita que estes copiando y pegando la "estructura de un objeto literal", 
+//si vamos a usar varias instancias de ese tipo que estemos creando, nos conviene usar una función constructora
+*/
+
+/*
+    EJERCICIO 2: FUnción constructora donde asignamos los metodos al prototipo, no a la función como tal 
+//FUnción constructora donde asignamos los metodos al prototipo, no a la función como tal
+function Animal(nombre, genero) {
+    this.nombre = nombre;
+    this.genero = genero;
+}
+//Estos son los metodos agregados al prototipo de la función constructora, esto va a generar, rendimiento y espacio
+//en memoria, si tu despliegas el objeto, te darás cuenta de que no aparecen los metodos, pero si aparecen en el "prototype"
+Animal.prototype.sonar = function() {
+    console.log("Haciendo un sonido y mi nombre es:"+" "+ this.nombre)
+}
+Animal.prototype.saludar = function() {
+    console.log("Hola, mi nombre es:"+" "+ this.nombre)
+}
+
 const Perro = new Animal("Snoopy", "Macho");
 const Gato = new Animal("Perlita", "Hembra");
+console.log(Perro)
+Perro.saludar();
+Perro.sonar();
 
-console.log(Perro);
-console.log(Gato);
-
-//28.57min
-
-
-
-
-
-
-//console.log(animal2);
+COnstructor:es un metodo especial que se ejecuta cuando se crea una nueva instancia de ese objeto 
+*/
