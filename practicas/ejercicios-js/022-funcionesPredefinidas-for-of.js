32f59cb9c148391d4f1f2e18635d3c9477ce6699
ProfeRickLinux <ricgarridoc@hotmail.com>
2024-04-25 14:55:39 -0600

    reordering my js exercises

---
+++ practicas/ejercicios-js/022-funcionesPredefinidas-for-of.js
@@ -0,0 +1,48 @@
+/*
+
+
+---------------------------------------------------------------------
+            FOR OF con funciones predefinidas
+
+const ciudades = ['Mexico', 'Canada', 'USA', 'Groelandia', 'Brasil'];
+for (let entrada of ciudades.values()) {
+    console.log(entrada);
+}
+
+Ejercicio 5, imprime los valores de un array usando FOR OF y la función .values()
+const ciudades = ['Mexico', 'Canada', 'USA', 'Groelandia', 'Brasil'];
+for (let entrada of ciudades.values()) {
+    console.log(entrada);
+}
+
+EJERCICIO 6, nos regresa solo los valores del Map, pero no los indices(llaves fue lo que dijo el profe)
+const datos = new Map();
+datos.set("nombre", "juan");
+datos.set("profesion", "desarrollo web");
+
+for (const entrada of datos.values()) {
+    console.log(entrada);
+}
+
+Ejercicio 7, usamos la función .keys() con FOR OF para conseguir el indice(o key dijo el profe)
+const ciudades = ['Mexico', 'Canada', 'USA', 'Groelandia', 'Brasil'];
+for (let entrada of ciudades.keys()) {
+    console.log(entrada);
+}
+
+Ejercicio 8, nos va a regresar nuestro indice pero en este caso el índice es nombre y profesión PORQUE ES UN OBJETO
+const datos = new Map();
+datos.set("nombre", "juan");
+datos.set("profesion", "desarrollo web");
+
+for (const entrada of datos.keys()) {
+    console.log(entrada);
+}
+
+EJERCICIO 9, imprime con un for, arrays creados con la función .entries(), usando los espacios de memoria y datos de 1 solo array
+const ciudades = ['Mexico', 'Canada', 'USA', 'Groelandia', 'Brasil'];
+for (let entrada of ciudades.entries()) {
+    console.log(entrada);
+}
+
+*/
--- practicas/ejercicios-js/024-Funciones-For.js
+++ practicas/ejercicios-js/024-Funciones-For.js
@@ -1,55 +1,5 @@
 /*
-
-Ejercicio 1.3, Hacer el ejercicio 1 pero con una función que cuente cuantos espacios tiene el array y que los imprima
-let countries = ['Mexico', 'USA', 'Canada', 'Japan', 'SOuth Korea'];
-for (let i = 0; i < countries.length; i++) {
-    console.log(countries[i]);
-}
-
-Ejercicio 20 Imprimir los objetos dentro de un array usando For
-const autos = [
-    { modelo: "Mustang", anio: 1962 },
-    { modelo: "360z", anio: 2010 },
-    { modelo: "Civic", anio: 2007 }
-];
-
-for (let i = 0; i < autos.length; i++) {
-    console.log(autos[i]);
-}
-
-EJERCICIO 21 Imprimir los objetos dentro del array con for pero imprimiendo nombre y modelo
-const autos = [
-    { modelo: "Mustang", anio: 1962 },
-    { modelo: "360z", anio: 2010 },
-    { modelo: "Civic", anio: 2007 }
-];
-
-for (let i = 0; i < autos.length; i++) {
-    console.log(autos[i].modelo + ' del año ' + autos[i].anio);
-}
-
-EJERCICIO 22 Hacer el ejercicio 30 pero imprimiendo con interpolacion
-const autos = [
-    { modelo: "Mustang", anio: 1962 },
-    { modelo: "360z", anio: 2010 },
-    { modelo: "Civic", anio: 2007 }
-];
-
-for (let i = 0; i < autos.length; i++) {
-    console.log(`${autos[i].modelo} del año ${autos[i].anio}`);
-}
-
-Ejercicio 23 Agregaga un objeto a un array const e imprimelo
-const autos = [
-    { modelo: "Mustang", anio: 1962 },
-    { modelo: "360z", anio: 2010 },
-    { modelo: "Civic", anio: 2007 }
-];
-
-autos.push({ modelo: "Mercedez Benz", anio: 2016 });
-for (let i = 0; i < autos.length; i++) {
-    console.log(autos[i].modelo + 'del año' + autos[i].anio);
-}
+AQUÍ ME QUEDE
 
 EJERCICIO 24 Modifica objeto dentro de un array const desde afuera(es la unica manera en la que se puede).
 const autos = [
