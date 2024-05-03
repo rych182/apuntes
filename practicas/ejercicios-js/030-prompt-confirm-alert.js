baad82d99499e131d4ea9a296ef2b1193f2e5b40
ProfeRickLinux <ricgarridoc@hotmail.com>
2024-05-03 14:18:14 -0600

    adding new js exercises

---
+++ practicas/ejercicios-js/030-prompt-confirm-alert.js
@@ -0,0 +1,11 @@
+/*
+Todos estos metodos que se encuentran en el objeto "window", los puedes envocar así: window.alert("") o solo alert("")
+*/
+
+let alerta = alert("HOla mundo");
+let confirmacion = confirm('Si das CLICK en "aceptar", el valor que guardaré será, TRUE y si das click en "cancelar", guardare FALSE ');
+let aviso = prompt('Lo que escribas se guardará en la variable, si das cancelar, se guardará el valor "null" ')
+
+console.log(alerta);
+console.log(confirmacion);
+console.log(aviso);
