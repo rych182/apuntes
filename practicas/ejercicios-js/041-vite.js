4141178577eda0025885cd9c90158e7b3c7ceae6
Ricardo Garrido <ricgarridoc@hotmail.com>
2024-09-25 22:51:43 -0600

    Adding new projects

---
+++ practicas/ejercicios-js/041-vite.js
@@ -0,0 +1,11 @@
+//checar la carpeta 041, ahí estan los apuntes e imagenes
+
+/*
+
+Se inicializa el proyecto:
+1- comando "npm create vite"
+2-elegir vanilla javascript y typescript
+3-le pones nombre a la carpeta del proyecto
+4-pones el comando npm install en la terminal, esto decargara la carpeta node_modules
+para que funcione vite
+*/
