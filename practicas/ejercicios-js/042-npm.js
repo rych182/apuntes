3686e87ee8a8f014eca007969651e824a08d6cdf
Ricardo Garrido <ricgarridoc@hotmail.com>
2024-10-05 10:30:20 -0600

    Terminando proyecto de ventana modal y menu de hamburguesa

---
+++ practicas/ejercicios-js/042-npm.js
@@ -0,0 +1,14 @@
+/*
+Comandos:
+npm init (para comenzar)
+npm init -y (para ponerle si a todo, como si dieras puros enter, generara el archivo package.json)
+npm run test echo (te imprime el mensaje que está en la sección de test)
+
+scripts
+
+Existe una sección de scripts dentro dl archivo "package.json" que sirve para correr tu proyecto ya sea en modo desarrollo
+o en modo de producción.
+
+
+
+*/
