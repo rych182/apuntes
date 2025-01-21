1d7dfcc3ec2eaeec14b8f4484ca737282b47d9f0
proferick <ricgarridoc@hotmail.com>
2025-01-20 20:15:12 -0600

    Promise exercises

--- practicas/ejercicios-js/037-promesas.js
+++ practicas/ejercicios-js/037-promesas.js
@@ -1,32 +1,48 @@
+const posts = [
+  {
+      userId: 1,
+      id: 1,
+      title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+  },
+  {
+      userId: 1,
+      id: 2,
+      title: "qui est esse",
+      body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+  },
+  {
+      userId: 1,
+      id: 3,
+      title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+      body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+  },
+];
 
-function firstOperation(callback) {
+
+// Función para encontrar un post por ID usando un callback
+function findPostById(id, callback) {
+  const post = posts.find((item) => item.id === id);
+
+  // Simulación de asincronía
   setTimeout(() => {
-    console.log("Primera operación completada");
-    callback();
+    if (post) {
+      callback(null, post); // Primer argumento es null para indicar que no hay error
+    } else {
+      callback("No encontrado por id: " + id, null); // Primer argumento es el error
+    }
   }, 1000);
 }
 
-function secondOperation(callback) {
-  setTimeout(() => {
-    console.log("Segunda operación completada");
-    callback();
-  }, 1000);
-}
 
-function thirdOperation(callback) {
-  setTimeout(() => {
-    console.log("Tercera operación completada");
-    callback();
-  }, 1000);
-}
+// USO DE CALLBACK HELL
+findPostById(1, (error, post1) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(post1);
 
-// Uso de callbacks anidados (callback hell)
-firstOperation(() => {
-  secondOperation(() => {
-    thirdOperation(() => {
-      console.log("Todas las operaciones completadas");
-    });
-  });
+  }
 });
 
 
@@ -148,6 +164,22 @@ myPromise
 
 ----------------------------------------------------------------------------
 Ejercicio 0: ejercicio corto de una promesa
+
+COMO SERIA EN CALLBACK
+
+function miOperacionConCallback(callback) {
+  // Simulamos una operación asíncrona
+  setTimeout(() => {
+    callback(42); // Llamamos al callback con el valor 42
+  }, 1000); // Retraso de 1 segundo
+}
+
+// Uso del callback
+miOperacionConCallback((resultado) => {
+  console.log("Resultado:", resultado); // Imprime: Resultado: 42
+});
+
+=======================================
 
 // Ejemplo de promesa simple
 let miPromesa = new Promise((resolve, reject) => {
@@ -325,9 +357,9 @@ asincroniaConCallBack(1,2,(parametro)=>{
 
 asincroniaConPromesas(3,4)
   .then(resultado => console.log(resultado))
-------------------------------------------------------------
-Ejercicio 5: haciendo el ejercicio 22 del archivo 034-callbacks.js
-que es un callback hell
+---------------------------------------------------------------------------------------------------------------------------------------
+Ejercicio 5: Hacer un programa que use la función callback .find() y use promesas, el programa debe de buscar los id dentro de los objetos del
+array(toma de ejemplo el callback hell)
 
 const posts = [
   {
@@ -349,6 +381,49 @@ const posts = [
       body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
   },
 ];
+
+
+// Función para encontrar un post por ID usando un callback
+function findPostById(id, callback) {
+  const post = posts.find((item) => item.id === id);
+
+  // Simulación de asincronía
+  setTimeout(() => {
+    if (post) {
+      callback(null, post); // Primer argumento es null para indicar que no hay error
+    } else {
+      callback("No encontrado por id: " + id, null); // Primer argumento es el error
+    }
+  }, 1000);
+}
+
+
+// USO DE CALLBACK HELL
+findPostById(1, (error, post1) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(post1);
+
+    findPostById(2, (error, post2) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(post2);
+
+        findPostById(3, (error, post3) => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log(post3);
+            console.log("Fin de las búsquedas con callback hell");
+          }
+        });
+      }
+    });
+  }
+});
+
 
 //Con la "promesa" no te tienes que estar inventando el NULL
 
@@ -384,49 +459,7 @@ findPostById(3)
   .catch( (err) => console.log(err) )
   .finally( () => console.log("fin de la promesa") )
 
-------------------------------------------------------------------------
-EJERCICIO 6:Refactorizando el ejercicio anterior( el 5 que es una promesa)
-
-//HAY FUNCIONES QUE RETORNAN DE INMEDIATO LA PROMESA
-//Esto no va a resultar porque estamos pidiendo un ID, pero así te lo puedes llegar a encontrar en otros códigos
-//const findPostById = new Promise( (resolve, reject) => 
-
-
-const posts = [
-  {
-      userId: 1,
-      id: 1,
-      title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-  },
-  {
-      userId: 1,
-      id: 2,
-      title: "qui est esse",
-      body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
-  },
-  {
-      userId: 1,
-      id: 3,
-      title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-      body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
-  },
-];
-
-const findPostById = (id) => new Promise( (resolve, reject) => {
-  const post = posts.find( (item) => item.id === id);
-  if (post) {
-    resolve(post);
-  } else {
-    reject("no se encontro id" + id)
-  }
-})
-
-findPostById(3)
-  .then( (post) => console.log(post) )
-  .catch( (err) => console.log(err) )
-
-  --------------------------------------------------------------------------------
+  -------------------------------------------------------------------------------------------------------------------------------------
 
 Ejercicio 7: Ejemplo de PROMISES HELL
 
@@ -474,7 +507,7 @@ findPostById(1)
     return findPostById(4)
   })
   .catch( (err) => console.log(err) )
------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------
 
 Ejercicio 8: Ejercicio de Jon Mircha
 Ejercicio donde atrapamos el error en cualquiera de las ejecuciones
