4b103fd9819bb28350e9abe4e087b98537cfb336
proferick <ricgarridoc@hotmail.com>
2025-03-14 16:08:49 -0600

    adding a new project with html,css,js

--- practicas/ejercicios-css-js/hamburguesa-sticky/index.js
+++ practicas/ejercicios-css-js/hamburguesa-sticky/index.js
@@ -2,5 +2,8 @@ const hamburguesa = document.querySelector('#hamburguer')
 const enlaces = document.querySelector('#nav-links')
 
 hamburguesa.addEventListener('click', ()=>{
+    //classList: Permite la manipulación del atributo de contenido de clase del elemento
+    //toggle: Si no se especifica la fuerza, se activa o desactiva el token, eliminándolo si está presente y agregándolo si no. Si la fuerza es verdadera, se agrega el token (igual que `add()`). Si la fuerza es falsa, se elimina el token (igual que `remove()`).
+    //Devuelve `true` si el token ya está presente y `false` en caso contrario.
     enlaces.classList.toggle('show')
 })
