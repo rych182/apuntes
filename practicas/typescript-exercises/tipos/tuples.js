efba59099c72edf1f5f28a095607517f838679ec
proferick <ricgarridoc@hotmail.com>
2025-12-29 21:21:39 -0600

    mew data types in Typescript

---
+++ practicas/typescript-exercises/tipos/tuples.js
@@ -0,0 +1,10 @@
+//le indico que mi primer datos sea X y el segundo Y
+const persona = ["ric", 38, true];
+//no acepta los datos porque no son los que espera en el orden
+//persona[0] = 0;
+//persona[1] = "raul"
+persona[0] = "raul";
+persona[1] = 50;
+console.log(persona);
+export {};
+//# sourceMappingURL=tuples.js.map
