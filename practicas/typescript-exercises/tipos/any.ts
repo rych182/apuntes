cf1776c4a7d9bb5425a4f67edf89d8d193ed6749
proferick <ricgarridoc@hotmail.com>
2025-12-29 18:11:53 -0600

    Practicing data types in Typescript

---
+++ practicas/typescript-exercises/tipos/any.ts
@@ -0,0 +1,18 @@
+let edad: any = 123;
+let monto: any = 123.4567;
+let variable2: number;
+
+edad = "lo que sea"
+//variable2 = 12;
+console.log(edad.charAt(0))
+console.log(monto.toFixed(2))
+//console.log(variable2);//Me marca error por que se le tiene que meter un número y no le metimos nada y es undefined
+
+//Para hacer casting(cambiar el tipo de dato)
+//console.log((edad as string).charAt(0)) EL MÁS UTILIZADO
+
+//Otro tipo de casting
+console.log((<number>monto).toFixed(2));
+//También funciona sin parentesis
+console.log(<number>monto.toFixed(2));
+
